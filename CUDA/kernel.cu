62f23845c54ed561c9d40d332999a3df2a555bb2
DanielFay22 <dpf2@rice.edu>
2018-12-03 21:47:25 -0600

    Fixes CUDA implementation

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -1,11 +1,11 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "cuda.h"
 
 #include <stdio.h>
 #include <math.h>
 #include <iostream>
-#include "kernel.h"
 
 #ifdef __CUDACC__
 	#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
@@ -20,8 +20,16 @@
 using namespace std;
 
 #define NUM_BITS ((unsigned int)(8 * sizeof(unsigned int)))
+#define CUDACHECK(cmd) do { \
+    cudaError_t e = cmd;    \
+    if( e != cudaSuccess ) { \
+    printf("Failed: Cuda error %s:%d '%s'\n", \
+        __FILE__,__LINE__,cudaGetErrorString(e)); \
+    exit(EXIT_FAILURE);     \
+  } \
+} while(0)
 
-const unsigned int data_length = 256;
+unsigned int data_length = 256;
 
 const unsigned int THREADS = 256;
 const unsigned int FILTER_LENGTH = 3;
@@ -52,6 +60,9 @@ void convEncode(const unsigned int *data, unsigned int *output) {
 		: 0))
 		| (data[idx / NUM_BITS + start] >> (NUM_BITS - 1 - (idx % NUM_BITS))))
 		& ((1 << FILTER_LENGTH) - 1));*/
+	__shared__ char bits[THREADS];
+
+	bits[idx] = 0;
 
 	for (unsigned int i = 0; i < NUM_FILTERS; i++) {
 
@@ -62,8 +73,21 @@ void convEncode(const unsigned int *data, unsigned int *output) {
 		n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
 		unsigned int newVal = (((((n + (n >> 4) & 0x0F0F0F0F) * 0x01010101) >> 24) & 1));
 
-		output[NUM_FILTERS * start + (NUM_FILTERS * idx) / NUM_BITS] |=
-			((unsigned int)(newVal << ((NUM_BITS * NUM_FILTERS - 1 - (NUM_FILTERS * (idx % NUM_BITS) + i)))));
+		bits[idx] |= (newVal << (NUM_FILTERS - 1 - i));
+	}
+
+	__syncthreads();
+
+	if (idx == 0) {
+		for (int i = 0; i < THREADS; i++) {
+			output[NUM_FILTERS * start + (NUM_FILTERS * i) / NUM_BITS] |=
+				((unsigned int)(bits[i]) & ~(1 << NUM_FILTERS)) << (NUM_BITS - NUM_FILTERS - ((i * NUM_FILTERS) % NUM_BITS));
+			//<< ((NUM_BITS * NUM_FILTERS - 1 - (NUM_FILTERS * (idx % NUM_BITS)))
+		}
+	}
+
+	//output[NUM_FILTERS * start + (NUM_FILTERS * idx) / NUM_BITS] |=
+	//	((unsigned int)(newVal << ((NUM_BITS * NUM_FILTERS - 1 - (NUM_FILTERS * (idx % NUM_BITS) + i)))));
 
 		//printf("%x\n", (newVal << ((NUM_BITS * NUM_FILTERS - 1 - (NUM_FILTERS * (idx % NUM_BITS) + i)))));
 	}
@@ -86,7 +110,7 @@ void convEncode(const unsigned int *data, unsigned int *output) {
 			((n + (n >> 4) & 0xF) * 0x01010101) >> 24) & 1)
 			<< (16 - (2 * (idx % 8) + i));
 	}*/
-}
+
 
 void printBitString(const unsigned int *data, int length) {
 	//ostringstream strStream;
@@ -98,50 +122,63 @@ void printBitString(const unsigned int *data, int length) {
 	cout << endl;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	unsigned int fillVal = 0xFFFFFFFF;
+
+	if (argc > 1) {
+		fillVal = (unsigned int)(strtol(argv[1], NULL, 0));
+	}
+	if (argc > 2) {
+		// Parse the provided data length, rounding up to the nearest multiple of NUM_BITS
+		data_length = (NUM_BITS - 1 + (unsigned int)(strtol(argv[2], NULL, 0))) / NUM_BITS * NUM_BITS;
+	}
+
+	cout << data_length << endl;
+
 	unsigned int *A, *B;
 
-	printf("%x\n", ((1 << FILTER_LENGTH) - 1));
+	//printf("%x\n", ((1 << FILTER_LENGTH) - 1));
 	
-	cudaMallocManaged(&A, dataArrayLength * sizeof(unsigned int));
-	cudaMallocManaged(&B, resultArrayLength * sizeof(unsigned int));
+	CUDACHECK(cudaMallocManaged(&A, dataArrayLength * sizeof(unsigned int)));
+	CUDACHECK(cudaMallocManaged(&B, resultArrayLength * sizeof(unsigned int)));
 
 	for (unsigned int i = 0; i < dataArrayLength; i ++) {
-		A[i] = 0xFFFFFFFF;
+		A[i] = fillVal;
 	}
 
 	for (unsigned int i = 0; i < resultArrayLength; i++) {
 		B[i] = 0;
 	}
 
-	cout << resultArrayLength << endl;
+	//cout << resultArrayLength << endl;
 
-	printBitString(A, dataArrayLength);
-	printBitString(B, resultArrayLength);
+	//printBitString(A, dataArrayLength);
+	//printBitString(B, resultArrayLength);
 
 	// This macro is used because the compiler throws a compile error when trying to parse 
 	// the correct CUDA kernel syntax
 	convEncode KERNEL_ARGS2((data_length + THREADS - 1) / THREADS, THREADS) (A, B);
 
-	cudaDeviceSynchronize();
+	CUDACHECK(cudaDeviceSynchronize());
 
-	int errors = 0;
+	//int errors = 0;
 
 	for (int i = 0; i < resultArrayLength; i ++) {
 		printf("%x", B[i]);
 	}
+	cout << endl;
 
-	unsigned int expected[] = { 0xca, 0x60000000 };
+	//unsigned int expected[] = { 0xca, 0x60000000 };
 
-	for (int i = 0; i < resultArrayLength; i++) {
-		errors += fmin((B[i]) ^ expected[i % 2], 1);
-	}
+	//for (int i = 0; i < resultArrayLength; i++) {
+	//	errors += ((B[i]) ^ expected[i % 2])  == 0 ? 0 : 1;
+	//}
 
-	std::cout << "\nTotal Error: " << errors << std::endl;
+	//std::cout << "\nTotal Error: " << errors << std::endl;
 
 	printBitString(B, resultArrayLength);
 
-	printBitString(A, dataArrayLength);
+	//printBitString(A, dataArrayLength);
 
 	cudaFree(A);
 	cudaFree(B);
